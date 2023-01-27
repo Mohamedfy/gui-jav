663b9e241bc8553eb6098b957a443a79b60dbeeb
Mohamedfy <93595650+Mohamedfy@users.noreply.github.com>
2023-01-27 22:17:38 +0200

    Add files via upload

---
+++ coffee shop/src/coffee/shop/CoffeeShop.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coffee.shop;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author mahmo
+ */
+public class CoffeeShop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame m= new coffee();
+        m.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
